5f76dc03f8f05de22939050bc3106ba98d806f71
Lindan Thillanayagam <lindthilldev@gmail.com>
2024-05-28 23:52:15 -0400

    Generated metro.config.js.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import StorybookUI from './.storybook'
+
+const Main: React.FC = () => {
+  return <View style={styles.container}>
+  <Text>Open up App.tsx to start working on your app!</Text>
+  <StatusBar style="auto" />
+</View>
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const STORYBOOK_ON: string | undefined = Constants.expoConfig?.extra?.STORYBOOK_ON;
+  return STORYBOOK_ON === 'true' ? StorybookUI : <Main />;
 }
 
 const styles = StyleSheet.create({
---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default {
+    expo: {
+        name: "STutorial",
+        slug: "s-tutorial",
+        version: "1.0.0",
+        extra: {
+			STORYBOOK_ON: process.env.STORYBOOK_ON,
+		}
+    }
+}
