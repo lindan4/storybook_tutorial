4d5a5961f0adcdfd03448995f7b79e52163dbb94
Lindan Thillanayagam <lindthilldev@gmail.com>
2025-01-11 17:11:37 -0500

    Updated scripts and metro config.

--- .storybook/stories/Button/Button.tsx
+++ .storybook/stories/Button/Button.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 
 export type MyButtonProps = {
--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const path = require("path");
 const { getDefaultConfig } = require("expo/metro-config");
-
-const { generate } = require("@storybook/react-native/scripts/generate");
-
-const withStorybook = require("@storybook/react-native/metro/withStorybook");
-
-generate({
-  configPath: path.resolve(__dirname, "./.storybook"),
-});
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.transformer.unstable_allowRequireContext = true;
-
-config.resolver.sourceExts.push("mjs");
+const withStorybook = require("@storybook/react-native/metro/withStorybook");
 
 module.exports = withStorybook(config);
